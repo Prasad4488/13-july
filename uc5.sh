47a164a17ee67ca9b7f5de235ed701718ad9c019
Prasad4488 <tepugadeprasad@gmail.com>
2022-07-14 19:34:28 +0530

    compute uc5 commit

---
+++ uc5.sh
@@ -0,0 +1,7 @@
+read -p "enter first input " a
+read -p "enter first input " b
+read -p "enter first input " c
+echo "First input is "$a
+echo "Second input is "$b
+echo  "Third input is "$c
+echo $(( $a%$b+$c ))
